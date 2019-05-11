56302cb9ae466b117fa51cab62c410cc9d7a13f8
Felix Diel <marvi3@gmx.net>
2019-05-11 22:33:53 +0200

    working state of db and core

--- src/clarity/database/in_memory_db/folder_item.py
+++ src/clarity/database/in_memory_db/folder_item.py
@@ -1,6 +1,6 @@
-from storage_item import storage_item
+from storage_item import Storage_item
 
-class folder_item(item):
+class folder_item(Storage_item):
 
     def __init__(self, name, directory, tag_list):
         super().__init__(self, name, directory, tag_list)
