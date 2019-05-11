4737e49bf708b655044b433108cc3a294aa1ae27
bela127 <bela127@hotmail.de>
2019-05-11 15:45:33 +0200

    Update test_console.py

--- src/Frontends/Console/test_console.py
+++ src/Frontends/Console/test_console.py
@@ -14,79 +14,146 @@ class Test_console(cmd.Cmd):
     # ----- basic function commands -----
     def do_help(self, arg):
         print("helpfull text")
-    
-    def do_list_all_tags(self,args):
-        pass
-    
-    def do_list_usefull_tags(self,args):
-        pass
-    
-    def do_list_files(self, args):
-        pass
-    
-    def do_show_path(self, args):
+
+    ## SEARCH
+    # SEARCH helpers
+
+    def do_list_current_tags(self, args):
         pass
 
-    def do_list_exact_files(self, args):
+    def do_get_last_tag(self, args):
         pass
+
+    def do_change_search_tag(self, args):
+        pass
+
+    def do_replace_search_tag(self, args):
+        pass
+
+    # LIST predictions
+    
+    def do_list_tag_predictions(self,args):
+        pass
+
+    def do_list_file_predictions(self,args):
+        pass
+
+    def do_list_folder_predictions(self,args):
+        pass
+    
+    # List Tags with text
 
     def do_list_tags_with_text(self, args):
         pass
     
-    def do_list_tags_with_tag(self, args):
+    def do_list_tags_start_with_text(self, args):
+        pass
+
+    # List Tags with querry
+
+    def do_list_tags_with_querry(self, args):
+        pass
+
+    ##
+
+    # SET Tags
+
+    def do_set_item_tag(self, args):
+        pass
+
+    def do_set_tag_tag(self, args):
         pass
     
     def do_set_file_tag(self, args):
+        pass
+    
+    def do_set_folder_tag(self, args):
+        pass
 
-    # ----- basic turtle commands -----
-    def do_forward(self, arg):
-        'Move the turtle forward by the specified distance:  FORWARD 10'
-        forward(*parse(arg))
-    def do_right(self, arg):
-        'Turn turtle right by given number of degrees:  RIGHT 20'
-        right(*parse(arg))
-    def do_left(self, arg):
-        'Turn turtle left by given number of degrees:  LEFT 90'
-        left(*parse(arg))
-    def do_goto(self, arg):
-        'Move turtle to an absolute position with changing orientation.  GOTO 100 200'
-        goto(*parse(arg))
-    def do_home(self, arg):
-        'Return turtle to the home position:  HOME'
-        home()
-    def do_circle(self, arg):
-        'Draw circle with given radius an options extent and steps:  CIRCLE 50'
-        circle(*parse(arg))
-    def do_position(self, arg):
-        'Print the current turtle position:  POSITION'
-        print('Current position is %d %d\n' % position())
-    def do_heading(self, arg):
-        'Print the current turtle heading in degrees:  HEADING'
-        print('Current heading is %d\n' % (heading(),))
-    def do_color(self, arg):
-        'Set the color:  COLOR BLUE'
-        color(arg.lower())
-    def do_undo(self, arg):
-        'Undo (repeatedly) the last turtle action(s):  UNDO'
-    def do_reset(self, arg):
-        'Clear the screen and return turtle to center:  RESET'
-        reset()
-    def do_bye(self, arg):
-        'Stop recording, close the turtle window, and exit:  BYE'
-        print('Thank you for using Turtle')
-        self.close()
-        bye()
-        return True
+    # ADD items
+    
+    def do_add_tag(self, args):
+        pass
 
-    # ----- record and playback -----
-    def do_record(self, arg):
-        'Save future commands to filename:  RECORD rose.cmd'
-        self.file = open(arg, 'w')
-    def do_playback(self, arg):
-        'Playback commands from a file:  PLAYBACK rose.cmd'
-        self.close()
-        with open(arg) as f:
-            self.cmdqueue.extend(f.read().splitlines())
+    def do_add_file(self, args):
+        pass
+
+    def do_add_folder(self, args):
+        pass
+
+    # LIST items with tags
+
+    def do_list_items_with_tags(self, args):
+        pass
+
+    def do_list_tags_with_tags(self, args):
+        pass
+
+    def do_list_files_with_tags(self, args):
+        pass
+    
+    def do_list_folders_with_tags(self, args):
+        pass
+
+    # LIST items with tag
+
+    def do_list_items_with_tag(self, args):
+        pass
+
+    def do_list_tags_with_tag(self, args):
+        pass
+
+    def do_list_files_with_tag(self, args):
+        pass
+    
+    def do_list_folders_with_tag(self, args):
+        pass
+
+    # LIST items
+
+    def do_list_all_files(self, args):
+        pass
+
+    def do_list_all_folders(self, args):
+        pass
+
+    def do_list_all_tags(self, args):
+        pass
+
+    # LIST exact items
+    
+    def do_list_exact_files(self, args):
+        pass
+
+    def do_list_exact_folders(self, args):
+        pass
+
+    def do_list_exact_tags(self, args):
+        pass
+
+    # LIST Tags
+
+    def do_list_tags_of_item(self, args):
+        pass
+
+    def do_list_tags_of_tags(self, args):
+        pass
+    
+    def do_list_tags_of_file(self, args):
+        pass
+
+    def do_list_tags_of_folder(self, args):
+        pass
+
+    # MODULE stuff
+    def do_list_all_modules(self, args):
+        pass
+
+    def do_call_module(self, args):
+        pass
+
+    #TODO more module stuff... help etc
+
     def precmd(self, line):
         line = line.lower()
         if self.file and 'playback' not in line:
