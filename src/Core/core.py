fa22791049ee721ca62e015239b832d44c2a95d5
Felix Diel <marvi3@gmx.net>
2019-05-11 15:17:07 +0200

    added core declarations for DB-request

--- src/Core/core.py
+++ src/Core/core.py
@@ -12,3 +12,40 @@ class core():
 
     def run(self):
         pass
+    
+    def list_tags_start_with(text):
+        pass
+    
+    def list_tags_contain(text):
+        pass
+    
+    def list_items_with(tags):
+        pass
+
+    def list_files_with(tags):
+        pass
+
+    def list_folders_with(tags):
+        pass
+
+    def list_tags_from(item):
+        pass
+    
+
+    def set_tag(item, tags):
+        pass
+
+
+    def index_item(name, directory, tags):
+        pass
+    
+    def index_file(name, directory, tags):
+        pass
+
+    def index_folder(name, directory, tags):
+        pass
+
+    def create_tag(name, tags):
+        pass
+
+    
