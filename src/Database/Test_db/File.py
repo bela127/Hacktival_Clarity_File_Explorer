92e60f707bc3f203f55d326562c3558c3647b676
bela127 <bela127@hotmail.de>
2019-05-11 17:19:08 +0200

    Update File.py and tags.py

--- src/Database/Test_db/File.py
+++
@@ -1,9 +0,0 @@
-import persistent
-
-class File(persistent.Persistent):
-    def __init__(self, name, directory, tag_list):
-        self.name = name
-        self.directory = directory
-        self.tag_list = tag_list
-
-    
