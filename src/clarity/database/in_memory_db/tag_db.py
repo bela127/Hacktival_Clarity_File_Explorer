304991004d7cdfe5d47ececb2c4881bdb0978626
bela127 <bela127@hotmail.de>
2019-05-12 08:23:44 +0200

    change tag list to root tag

--- src/clarity/database/in_memory_db/tag_db.py
+++ src/clarity/database/in_memory_db/tag_db.py
@@ -3,12 +3,11 @@ from clarity.database.in_memory_db.tag_item import Tag_item
 class Tag_db():
     def __init__(self):
         self.root_tag = Tag_item("root")
-        self.tags: [Tag_item] = [self.root_tag]      #list of tags
-    
+            
     # returns all tags that start with the string text
     def tags_start_with(self, text):
         tag_list = []
-        for tag in self.tags:
+        for tag in self.root_tag.used_in_tag_items:
             if tag.name.startswith(text):
                 tag_list.append(tag)
     
@@ -16,11 +15,11 @@ class Tag_db():
 
     # returns all tags that contain the string text
     def tags_contain(self, text):
-        return [tag for tag in self.tags if text in tag.name]
+        return [tag for tag in self.root_tag.used_in_tag_items if text in tag.name]
 
     # returns a tag
     def return_tag(self, name):
-        for tag in self.tags:
+        for tag in self.root_tag.used_in_tag_items:
             if name == tag.name:
                 return tag
         return None
@@ -28,34 +27,34 @@ class Tag_db():
     # gets a name and a taglist and creates a tag
     def add_tag(self, name, tags):
         new_tag = Tag_item(name, tags)
-        self.tags.append(new_tag)
+        self.root_tag.used_in_tag_items.append(new_tag)
         for tag in tags:
             self.return_tag(tag.name).used_in_tag_items.append(new_tag)
     
     # gets an item and returns a list of tags of this item
     def list_tags_of_item(self, item):
-        for tag in self.tags:
+        for tag in self.root_tag.used_in_tag_items:
             if item.name == tag.name:
                 return tag.tag_list
         return[]
 
     # returns all existing tags
     def list_all_tags(self):
-        return self.tags
+        return self.root_tag.used_in_tag_items
 
     # gets a list of tags and returns all folders where these tags are used 
     def list_folders_with_tags(self, tag_list):
-        folders = set([folder for tag in self.tags for folder in tag.used_in_folder_items])
+        folders = set([folder for tag in tag_list for folder in tag.used_in_folder_items])
         folders = list(folders)
         return folders
 
     def list_files_with_tags(self, tag_list):
-        files = [file for tag in self.tags for file in tag.used_in_file_items]
+        files = [file for tag in tag_list for file in tag.used_in_file_items]
         files = list(set(files))
         return files
 
     def list_tags_with_tags(self, tag_list):
-        tags = [tag for tag in self.tags for tag in tag.used_in_tag_items]
+        tags = [tag for tag in tag_list for tag in tag.used_in_tag_items]
         tags = list(set(tags))
         return tags
 
