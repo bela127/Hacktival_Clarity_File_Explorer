3600939c614247ecd12ccdffc5f6433a44978223
bela127 <bela127@hotmail.de>
2019-05-12 13:08:39 +0200

    Update inputwithddanddelete.py

--- src/clarity/frontends/guis/pycute/inputwithddanddelete.py
+++ src/clarity/frontends/guis/pycute/inputwithddanddelete.py
@@ -6,8 +6,22 @@ Created on Sat May 11 17:55:05 2019
 """
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QLineEdit, QListWidget, QPushButton, QComboBox, QFrame, QGraphicsDropShadowEffect, QScrollArea
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QLineEdit, QListWidget, QPushButton, QFrame, QGraphicsDropShadowEffect, QScrollArea, QComboBox
 from FlowLayout import FlowLayout
+
+
+class DropDown (QComboBox):
+
+    def __init__(self, parent, onShowPopup):
+        super().__init__(parent)
+        self.onShowPopup = onShowPopup
+    
+
+    def showPopup(self):
+        self.onShowPopup()
+        super().showPopup()
+
+
 
 class InputItemWithDelete (QFrame):
     
@@ -27,7 +41,17 @@ class InputItemWithDelete (QFrame):
         shadow = QGraphicsDropShadowEffect(blurRadius=5, xOffset=3, yOffset=3)
         self.setGraphicsEffect(shadow)
         
-        self.dropdown = QComboBox(self)
+
+        def onDropDownClick():
+            text = self.dropdown.currentText()
+            tag = api.get_tag_by_name(text)
+            tags = api.list_all_tags()[:5] # TODO
+            items = [tag.name for tag in tags]
+            self.dropdown.clear()
+            self.dropdown.addItem(text)
+            self.dropdown.addItems(items)
+
+        self.dropdown = DropDown(self, onDropDownClick)
         self.dropdown.setStyleSheet("""
                                     border: none;
                                     border-radius: 2px;
@@ -38,18 +62,8 @@ class InputItemWithDelete (QFrame):
         
         self.cross = QPushButton('x', self)
         self.cross.setFixedWidth(20)
-
-
-        def onSelect():
-            text = self.dropdown.currentText()
-            tag = api.get_tag_by_name(text)
-            tags = api.list_all_tags()[:5] # TODO
-            items = [tag.name for tag in tags]
-            self.dropdown.clear()
-            self.dropdown.addItem(text)
-            self.dropdown.addItems(items)
         
-        self.dropdown.highlighted.connect(onSelect)
+        #self.dropdown.highlighted.connect(onSelect)
         #self.dropdown.addItems(['xD', '^^', ':P'])
 
         def onChange():
@@ -58,7 +72,6 @@ class InputItemWithDelete (QFrame):
             to_replace = api.get_tag_by_name(self.text)
             if new_tag and to_replace:
                 api.replace_search_tag(to_replace, new_tag)
-                print('' + self.text + ' -> ' + text)
                 self.text = text
         
         self.dropdown.activated.connect(onChange)
